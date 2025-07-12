541b8aa3f1b2fa3661e482f6176b3cd26fbf0fef
Murilo Arantes <murilo.arantess@gmail.com>
2025-07-12 14:16:17 -0300

    several improvements on logs script

--- scripts-sh/logs/logs.sh
+++ scripts-sh/logs/logs.sh
@@ -4,16 +4,16 @@
 
 function _get_log_by_day() {
     local dt_log=$(date '+%Y%m%d')
-    echo "${LOG_PATH}-${dt_log}.log"
+    printf "${LOGS_LIB_LOG_PATH}-${dt_log}.log"
 }
 
 function _get_log_by_hour() {
     local dt_log=$(date '+%Y%m%d-%H')
-    echo "${LOG_PATH}-${dt_log}.log"
+    printf "${LOGS_LIB_LOG_PATH}-${dt_log}.log"
 }
 
 function _get_log_stdout() {
-    echo '/dev/stdout'
+    printf '/dev/stdout'
 }
 
 function _write_log() {
@@ -32,67 +32,101 @@ function _write_log() {
 ### public functions
 
 function generate_trace_id() {
-    shuf -n 8 -r -e $(echo {a..z} {A..Z} {0..9} | tr ' ' '\n') | tr -d '\n'
+    shuf -n 8 -r -e {a..z} {A..Z} {0..9} | tr -d '\n'
 }
 
 function log_info() {
     local msg=$1
     local trace_id=$2
     _write_log 'INFO' "${msg}" "${trace_id}"
+    return 0
 }
 
 function log_error() {
     local msg=$1
     local trace_id=$2
     _write_log 'ERROR' "${msg}" "${trace_id}"
+    return 1
 }
 
-function is_ok_code_logs()  {
+function log_error_and_exit() {
+    local msg=$1
+    local trace_id=$2
+    log_error "${msg}" "${trace_id}"
+    exit "$?"
+}
+
+function is_ok_code_logs() {
     local code=$1
     local msg=$2
     local trace_id=$3
-    if [[ ${code} -eq 0 ]]; then
+    if [[ "${code}" -eq 0 ]]; then
         log_info "${msg}" "${trace_id}"
     fi
-    return ${code}
+    return "${code}"
 }
 
-function is_fail_code_logs()  {
+function is_fail_code_logs() {
     local code=$1
     local msg=$2
     local trace_id=$3
-    if [[ ${code} -ne 0 ]]; then
+    if [[ "${code}" -ne 0 ]]; then
         log_error "${msg}" "${trace_id}"
         return 0
     fi
     return 1
 }
 
-function is_ok_else_fail_logs_both() {
+function if_fail_code_logs_and_exit() {
+    local code=$1
+    local msg=$2
+    local trace_id=$3
+    if [[ "${code}" -ne 0 ]]; then
+        log_error "${msg}" "${trace_id}"
+        exit "${code}"
+    fi
+    return "${code}"
+}
+
+function is_ok_else_fail_code_logs() {
     local code=$1
     local msg_ok=$2
     local msg_fail=$3
     local trace_id=$4
-    if [[ ${code} -eq 0 ]]; then
+    if [[ "${code}" -eq 0 ]]; then
         log_info "${msg_ok}" "${trace_id}"
     else
         log_error "${msg_fail}" "${trace_id}"
     fi
-    return ${code}
+    return "${code}"
+}
+
+function if_ok_or_fail_code_logs_and_exit() {
+    local code=$1
+    local msg_ok=$2
+    local msg_fail=$3
+    local trace_id=$4
+    if [[ "${code}" -eq 0 ]]; then
+        log_info "${msg_ok}" "${trace_id}"
+        return 0
+    else
+        log_error "${msg_fail}" "${trace_id}"
+        exit "${code}"
+    fi    
 }
 
 function start_log_rotation() {
-    local days_retation=$1
-    if [[ ! "${days_retation}" =~ ^[0-9]+$ ]] || [[ "${days_retation}" -le 0 ]]; then
-        echo "invalid retation days parameter: ${days_retation}"
+    local log_retention_days=$1
+    if [[ ! "${log_retention_days}" =~ ^[0-9]+$ ]] || [[ "${log_retention_days}" -le 0 ]]; then
+        printf "invalid retation days parameter: ${log_retention_days}\n"
         return 1
     fi
     if [[ ! -d "${LOGS_LIB_LOG_FOLDER}" ]] || [[ -z "${LOGS_LIB_LOG_NAME}" ]]; then
-        echo 'invalid LOGS_LIB_LOG_FOLDER and LOGS_LIB_LOG_NAME variables'
+        printf 'invalid LOGS_LIB_LOG_FOLDER and LOGS_LIB_LOG_NAME variables\n'
         return 1
     fi
     while true; do
-        find ${LOGS_LIB_LOG_FOLDER} -type f -mtime +${days_retation} -delete
+        find ${LOGS_LIB_LOG_FOLDER} -type f -mtime +${log_retention_days} -delete
         sleep 60
     done
     return 0
@@ -101,24 +135,27 @@ function start_log_rotation() {
 ## call 'setup' before use the another public functions
 
 function setup_logger() {
-    if [[ -d "${LOGS_LIB_LOG_FOLDER}" ]] && [[ -n "${LOGS_LIB_LOG_NAME}" ]]; then
-        LOG_PATH="${LOGS_LIB_LOG_FOLDER}/${LOGS_LIB_LOG_NAME}"
-        echo 'test log permission' > ${LOG_PATH}
+    local log_folder="${LOGS_LIB_LOG_FOLDER:-}"
+    local log_name="${LOGS_LIB_LOG_NAME:-}"
+    local log_rotation="${LOGS_ROTATION:-day}"
+    if [[ -d "${log_folder}" ]] && [[ -n "${log_name}" ]]; then
+        LOGS_LIB_LOG_PATH="${log_folder}/${log_name}"
+        touch "${LOGS_LIB_LOG_PATH}.test"
         if [[ $? -ne 0 ]]; then
-            echo "writing log failed: ${LOG_PATH}"
+            printf "writing log failed: ${LOGS_LIB_LOG_PATH}\n"
             return 1
         else
-            rm -f ${LOG_PATH}
+            rm -f "${LOGS_LIB_LOG_PATH}.test"
         fi
-        if [[ "${LOGS_ROTATION}" == "hour" ]]; then
-            echo 'logger setup to file by hour'
+        if [[ "${log_rotation}" == "hour" ]]; then
+            printf 'logger setup to file by hour\n'
             _func_get_log_path='_get_log_by_hour'
         else
-            echo 'logger setup to file by day'
+            printf 'logger setup to file by day\n'
             _func_get_log_path='_get_log_by_day'
         fi
     else
-        echo 'logger setup to stdout'
+        printf 'logger setup to stdout\n'
         _func_get_log_path='_get_log_stdout'
     fi
     return 0
